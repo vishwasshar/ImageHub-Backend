7ed996fc54c0e8ce2c18c484e4a0f5ae20e6f747
Vishwas Sharma <vishwassharma3287@gmail.com>
2023-09-10 17:11:47 +0530

    CORS Error in fileupload

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ app.use(
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Cache-Control, Key, Access-Control-Allow-Origin, X-File-Name, Authorization"
-  );
+  console.log(req.body);
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
